da58413f0dfad43a5d4d30c9becc594cb99ba482
CJ <weicongju@gmail.com>
2012-12-17 17:01:43 +0800

    适配器改名字了

--- tests/backends/oss/test_adapter.py
+++ tests/backends/oss/test_adapter.py
@@ -1,5 +1,5 @@
-from cloudstore.backends.oss import OSSAdapter
-from cloudstore import Bucket
+from cloudstore.backends.oss import Adapter
+from cloudstore import Bucket, OSSConfig
 from mock_oss_api import MockOssAPI
 from mock_handler import MockOSSBucketXMLContentHandler, MockOSSObjectXMLContentHandler, MockSaxParserFactory
 from mock_assembly import MockAssembly
@@ -7,7 +7,7 @@ from pdb import set_trace as bp
 
 class TestOSSAdapter:
 	def setUp ( self ):
-		self.adapter = OSSAdapter ( None, None, None )
+		self.adapter = Adapter ( OSSConfig ( None, None, None ) )
 		self.adapter.oss = MockOssAPI ( )
 		self.adapter.assembly = MockAssembly ( )
 
