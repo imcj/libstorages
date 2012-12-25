7532a55f507093cc4fd0743ec4233ac5308c417e
CJ <weicongju@gmail.com>
2012-12-25 23:36:12 +0800

    更新文档的工具

--- tools/update-website.py
+++ tools/update-website.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 
 import os
+import sys
+library_dir = os.path.abspath ( os.path.join ( \
+    os.path.dirname ( os.path.abspath ( __file__ ) ), \
+    ".." ) )
+
+sys.path.insert ( 0, library_dir )
+
 import cloudstore
+from pdb import set_trace as bp
 
 build_html = os.path.abspath ( os.path.join ( \
     os.path.dirname ( os.path.abspath ( __file__ ) ), \
     "..", "docs", "build", "html" ) )
+
+store = cloudstore.env ( "s3" )
+
 for path in os.walk ( build_html ):
     for file_path in path[2]:
         local_file = os.path.join ( path[0], file_path )
         remote_object_name = local_file [ len ( build_html ) + 1:] \
                              .replace ( "\\", "/" )
-        
-        cloudstore.create_object ( \
+
+        store.create_object_from_file ( \
             "cloudstore.bukaopu.us", \
-            open ( remote_object_name ) )
+            remote_object_name, \
+            local_file )
 
         print "Upload <%s> finish." % remote_object_name
