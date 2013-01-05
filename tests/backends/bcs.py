835853a6fb84fb2074860e30ccd3b543633949f6
CJ <weicongju@gmail.com>
2013-01-05 20:23:47 +0800

    修改API

--- tests/backends/bcs.py
+++ tests/backends/bcs.py
@@ -1,13 +1,13 @@
 #! -*- encoding: utf-8 -*-
 
 __doc__="""
->>> from libstorages import env, Store, BUCKET_PUBLIC, TEST_BUCKET_CREATE
+>>> from libstorages import env, Storage, BUCKET_PUBLIC, TEST_BUCKET_CREATE
 >>> from StringIO import StringIO
 >>> from pdb import set_trace as bp 
 >>> import tempfile
 >>> import os
->>> store = env ( "bcs" )
->>> isinstance ( store, Store )
+>>> storage = env ( "bcs" )
+>>> isinstance ( storage, Storage )
 True
 >>> loaded = []
 >>> total  = 0
@@ -16,40 +16,43 @@ True
 ...     global total
 ...     loaded.append ( l )
 ...     total = t
->>> store.create_object ( BUCKET_PUBLIC, "hello", "hello, world!!!", \
+>>> storage.create_object ( BUCKET_PUBLIC, "hello", "hello, world!!!", \
 upload_callback = upload_callback )
+<Object: hello>
 >>> total
 15
 >>> loaded
 [15]
->>> store.create_object ( BUCKET_PUBLIC, "hello2", \
+>>> storage.create_object ( BUCKET_PUBLIC, "hello2", \
 StringIO ( "hello, world!!!" ) )
+<Object: hello2>
 >>> temp = tempfile.NamedTemporaryFile ( )
 >>> temp.file.write ( "hello, world!!!" )
 >>> temp.file.close ( )
->>> store.create_object_from_file ( BUCKET_PUBLIC, "hello3", \
+>>> storage.create_object_from_file ( BUCKET_PUBLIC, "hello3", \
 temp.name )
->>> hello = store.get_object ( BUCKET_PUBLIC, "hello" )
+<Object: hello3>
+>>> hello = storage.get_object ( BUCKET_PUBLIC, "hello" )
 >>> hello.read ( 10 )
 'hello, wor'
 >>> hello.read ( 10 )
 'ld!!!'
->>> store.get_object ( BUCKET_PUBLIC, "hello2" ).read ( )
+>>> storage.get_object ( BUCKET_PUBLIC, "hello2" ).read ( )
 'hello, world!!!'
->>> store.get_object ( BUCKET_PUBLIC, "hello3" ).read ( )
+>>> storage.get_object ( BUCKET_PUBLIC, "hello3" ).read ( )
 'hello, world!!!'
->>> store.create_bucket ( TEST_BUCKET_CREATE )
+>>> storage.create_bucket ( TEST_BUCKET_CREATE )
 Traceback (most recent call last):
     ...
 BucketCanNotCreate
->>> store.delete_object ( BUCKET_PUBLIC, "hello" )
->>> store.get_object ( BUCKET_PUBLIC, "hello" ).read ( )
+>>> storage.delete_object ( BUCKET_PUBLIC, "hello" )
+>>> storage.get_object ( BUCKET_PUBLIC, "hello" ).read ( )
 Traceback (most recent call last):
     ...
 ObjectNotExists: hello
->>> store.get_all_buckets ( )
+>>> storage.get_all_buckets ( )
 [<Bucket: libstorages-cj-private>, <Bucket: libstorages-cj-public>, \
 <Bucket: cloudstore-test-create>, <Bucket: bukaopu2>, <Bucket: bukaopu>]
->>> store.get_all_objects ( BUCKET_PUBLIC )
+>>> storage.get_all_objects ( BUCKET_PUBLIC )
 [<Object: hello3>, <Object: hello2>]
 """
