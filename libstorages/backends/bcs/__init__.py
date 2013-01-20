ef1f3349994bef8fed34b2dc4ef5f7b3df1743e9
CJ <weicongju@gmail.com>
2013-01-20 16:57:07 +0800

    get_key方法

--- libstorages/backends/bcs/__init__.py
+++ libstorages/backends/bcs/__init__.py
@@ -1,4 +1,6 @@
 #! -*- encoding:utf-8 -*-
+import logging
+logging.getLogger ( "pyhttpclient" ).setLevel ( logging.CRITICAL )
 
 from libstorages import pybcs, Key, Bucket, Storage, KeyList
 from libstorages.errors import BucketNameDuplication, BucketCanNotCreate, \
@@ -7,6 +9,8 @@ from libstorages.pybcs.httpc import HTTPException
 from pdb import set_trace as bp
 from StringIO import StringIO
 
+import time
+import datetime
 import simplejson
 
 class BucketAssembly:
@@ -16,15 +20,33 @@ class BucketAssembly:
 class BCSKey ( Key ):
     def __init__ ( self, bcs_object, *args, **kwargs ):
         self.bcs_object = bcs_object
-        self.reading = False
+        self.reading  = False
+        self.loaded   = 0
         self.response = None
 
         if not kwargs.has_key ( "name" ):
             kwargs['name'] = self.bcs_object.object_name
         super ( BCSKey, self ).__init__ ( *args, **kwargs )
 
-    def create ( self, data ):
-        self.bcs_object.put ( data )
+    def create ( self, data, content_type = None, \
+                       metadata = None, expires = None, \
+                       upload_callback = None ):
+
+        content_type = not content_type and "binary/octet-stream"
+        # TODO expires是否是格式正确的GMT时间
+        header = { "Content-Type": content_type }
+        if expires:
+            header['Expires'] = expires
+        
+        if metadata:
+            for metadata in metadata:
+                try:
+                    header['x-bcs-meta-%s' % metadata['key']] = \
+                    metadata['value']
+                except KeyError:
+                    pass
+
+        self.bcs_object.put ( data, header )
 
     def create_from_buffer ( self, buff ):
         self.bcs_object.put ( buff )
@@ -43,15 +65,54 @@ class BCSKey ( Key ):
     def exists ( self ):
         pass
 
+    def _parse_datetime ( self, baidu_datetime ):
+        return datetime.datetime.strptime ( baidu_datetime, \
+                                            "%a, %d %b %Y %H:%M:%S GMT" )
+
+    def _parse_header ( self, header ):
+        self.header = header
+        self.content_type = self.header['content-type']
+        self.size = int ( self.header['content-length'] )
+        self.last_modified = self._parse_datetime ( \
+            self.header['last-modified'] )
+        self.etag = self.header['etag']
+        self.md5  = self.header['content-md5']
+        self.expires = self._parse_datetime ( self.header['expires'] )
+
+        for key in self.header.keys ( ):
+            try:
+                if not 'x-bs-meta-' == key[:10]:
+                    continue
+                short_key = key[10:]
+                if short_key == "crc32":
+                    value = int ( self.header[key] )
+                else:
+                    value = self.header[key]
+
+                self.meta[short_key] = value
+
+            except ValueError:
+                continue
+
+
     def read ( self, size = None ):
         if not self.reading:
-            self.reading = not self.reading
             try:
                 self.response = self.bcs_object.get ( )
+                self.reading = True
+                self._parse_header ( self.response['header'] )
+
             except HTTPException, e:
                 if 404 == e.status:
                     raise ObjectNotExists ( self.name[1:] )
-        
+
+        size = not size and self.size or size
+        self.loaded += size
+
+        if not self.loaded < self.size:
+            self.reading = False
+            self.loaded  = 0
+
         return self.response['body'].read ( size )
 
     def __repr__ ( self ):
@@ -77,10 +138,11 @@ class BCSStorage ( Storage ):
     def _factory_create_bucket ( self, bucket ):
         return BCSBucket ( self.bcs, bucket )
 
-     def _factory_create_key ( self, bucket, key, upload_callback = None ):
-        return BCSKey ( self.bcs.bucket ( bucket ).object ( "/" + key, upload_callback ) )
+    def _factory_create_key ( self, bucket, key, upload_callback = None ):
+        return BCSKey ( self.bcs.bucket ( bucket ).object ( "/" + \
+            key, upload_callback ) )
 
-   def create_bucket ( self, bucket ):
+    def create_bucket ( self, bucket ):
         return BCSBucket ( self.bcs, bucket ).create ( )
 
     def delete_bucket ( self, bucket ):
@@ -93,28 +155,32 @@ class BCSStorage ( Storage ):
             in buckets
         ]
 
-    def create_key ( self, bucket, key, data, upload_callback = None ):
-        bcs_object = BCSKey ( \
-            self._factory_create_key ( bucket, key, upload_callback ) )
+    def create_key ( self, bucket, key, data, content_type = None, \
+                     metadata = None, expires = None, \
+                     upload_callback = None ):
+        bcs_object = self._factory_create_key ( bucket, key, upload_callback ) 
         bcs_object.create ( data )
 
         return bcs_object
 
-    def create_key_from_file ( self, bucket, key, file_path, \
-                                  upload_callback = None ):
+    def create_key_from_file ( self, bucket, key, file_path, content_type = \
+                               None, metadata = None, expires = None, \
+                               upload_callback = None ):
         return self.create_key ( bucket, key, open ( file_path, 'r' ), \
                                     upload_callback )
 
-    def create_key_from_stream ( self, bucket, key, stream, \
-                                    upload_callback = None ):
+    def create_key_from_stream ( self, bucket, key, content_type = None, \
+                                 metadata = None, expires = None, \
+                                 upload_callback = None ):
         # TODO 修改bcs的代码，使其支持 file 对象。
-        self.create_object ( bucket, key, stream, upload_callback )
+        self.create_key ( bucket, key, stream, upload_callback )
 
 
     def get_key ( self, bucket, key ):
         try:
-            return BCSKey ( self.bcs.bucket ( bucket ) \
-                   .object ( "/" + key ) )
+            key = BCSKey ( self.bcs.bucket ( bucket ).object ( "/" + key ) )
+            key.read ( 0 )
+            return key
         except pybcs.httpc.HTTPException, e:
             if 404 == e.status:
                 raise ObjectNotExists ( )
--- tests/backends/bcs/bcs.py
+++ tests/backends/bcs/bcs.py
@@ -4,6 +4,8 @@ __doc__="""
 >>> from libstorages import env, Storage, BUCKET_PUBLIC, TEST_BUCKET_CREATE
 >>> from StringIO import StringIO
 >>> from pdb import set_trace as bp 
+>>> import time
+>>> import datetime
 >>> import tempfile
 >>> import os
 >>> storage = env ( "bcs" )
@@ -17,7 +19,10 @@ True
 ...     loaded.append ( l )
 ...     total = t
 >>> storage.create_key ( BUCKET_PUBLIC, "hello", "hello, world!!!", \
-upload_callback = upload_callback )
+upload_callback = upload_callback, content_type = "application/json", \
+metadata = [ { "name" : "hello", "length": 0 } ], expires = time.strftime ( \
+"%a, %d %b %Y %H:%M:%S +0000", time.gmtime ( time.time ( ) + \
+datetime.timedelta ( days = 5 ).total_seconds ( ) ) ) )
 <Key: hello>
 >>> total
 15
@@ -33,6 +38,10 @@ StringIO ( "hello, world!!!" ) )
 temp.name )
 <Key: hello3>
 >>> hello = storage.get_key ( BUCKET_PUBLIC, "hello" )
+>>> hello.meta['crc32'] > 1
+True
+>>> hello.etag == hello.md5 and len ( hello.md5 ) == 32
+True
 >>> hello.read ( 10 )
 'hello, wor'
 >>> hello.read ( 10 )
